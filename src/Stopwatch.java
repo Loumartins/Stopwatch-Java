fa27a67847c24cb35d80c05a9892a65824c79306
Luiz <luizhmartins11@gmail.com>
2022-12-21 10:40:05 -0300

    Criação dos metodos iniciais e implementação do ActionListener

---
+++ src/Stopwatch.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class Stopwatch implements ActionListener {
+
+    Stopwatch(){
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    void start() {
+
+    }
+    void stop() {
+
+    }
+    void reset(){
+
+    }
+}
