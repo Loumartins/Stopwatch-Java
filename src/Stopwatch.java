5f06ced10fa91be5b1fe7ffc9d019832e8b7e11f
Luiz <luizhmartins11@gmail.com>
2022-12-21 10:43:58 -0300

    Criação das variaveis e instancias de referencia do swing

--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -1,8 +1,17 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class Stopwatch implements ActionListener {
 
+    JFrame frame = new JFrame();
+    JButton startButton = new JButton("START");
+    JButton resetButton = new JButton("RESET");
+    JLabel timeLabel = new JLabel();
+    int elapsedTime = 0;
+    int seconds = 0;
+    int minutes = 0;
+    int hours = 0;
     Stopwatch(){
 
     }
