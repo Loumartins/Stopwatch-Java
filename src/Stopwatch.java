c26411408b9597d82b29890c3005ad3f7ac534d7
Luiz <luizhmartins11@gmail.com>
2022-12-21 10:49:17 -0300

    Utilização do frame no constructor e criação das String.format

--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -12,8 +12,15 @@ public class Stopwatch implements ActionListener {
     int seconds = 0;
     int minutes = 0;
     int hours = 0;
+    boolean started = false;
+    String seconds_string = String.format("%02d", seconds);
+    String minutes_string = String.format("%02d", minutes);
+    String hours_string = String.format("%02d", hours);
     Stopwatch(){
-
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,420);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
